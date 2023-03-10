08256fc63cd73fa0cb2efe0b7234be5d2a8b0139
Kholofelo Paka <kholofelo.paka@standardbank.co.za>
2023-03-10 11:30:09 +0200

    CORS allow all origin

--- pokemon-service/src/main/java/com/paka/pokemon/PokemonApplication.java
+++ pokemon-service/src/main/java/com/paka/pokemon/PokemonApplication.java
@@ -2,6 +2,9 @@ package com.paka.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PokemonApplication {
@@ -10,4 +13,17 @@ public class PokemonApplication {
         SpringApplication.run(PokemonApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                                "Access-Control-Request-Headers")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                        .allowCredentials(true).maxAge(3600);
+            }
+        };
+    }
 }
